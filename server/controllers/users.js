91e8efcca65361672fe8ecd5bc3b592a1f8c570c
Ricardo D. de Guzmán <rdguzman@tercerplaneta.com>
2020-02-20 17:01:09 -0300

    Change bcrypt module.

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,6 +1,6 @@
 let User = require("../models/user.model");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 exports.users_signup = (req, res) => {
@@ -9,24 +9,45 @@ exports.users_signup = (req, res) => {
     if (user.length >= 1) {
       res.status(409).json({ message: "El usuario ya fue dado de alta." });
     } else {
-      bcrypt.hash(req.body.password, 10, (err, hash) => {
-        if (err) {
-          return res.status(500).json({ message: "Contraseña inválida" });
-        } else {
-          const user = new User({
-            _id: new mongoose.Types.ObjectId(),
-            username: req.body.username,
-            password: hash,
-            isAdmin: req.body.isAdmin
-          });
-          user
-            .save()
-            .then(result =>
-              res.status(201).json({ message: "Usuario registrado." })
-            )
-            .catch(err => res.status(500).json({ error: err }));
-        }
+      bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(req.body.password, salt, function(err, hash) {
+          if (err) {
+            return res.status(500).json({ message: "Contraseña inválida" });
+          } else {
+            const user = new User({
+              _id: new mongoose.Types.ObjectId(),
+              username: req.body.username,
+              password: hash,
+              isAdmin: req.body.isAdmin
+            });
+            user
+              .save()
+              .then(result =>
+                res.status(201).json({ message: "Usuario registrado." })
+              )
+              .catch(err => res.status(500).json({ error: err }));
+          }
+        });
       });
+
+      // bcryptjs.hash(req.body.password, 10, (err, hash) => {
+      //   if (err) {
+      //     return res.status(500).json({ message: "Contraseña inválida" });
+      //   } else {
+      //     const user = new User({
+      //       _id: new mongoose.Types.ObjectId(),
+      //       username: req.body.username,
+      //       password: hash,
+      //       isAdmin: req.body.isAdmin
+      //     });
+      //     user
+      //       .save()
+      //       .then(result =>
+      //         res.status(201).json({ message: "Usuario registrado." })
+      //       )
+      //       .catch(err => res.status(500).json({ error: err }));
+      //   }
+      // });
     }
   });
 };
@@ -41,7 +62,11 @@ exports.users_login = (req, res) => {
           message: "Autenticacion invalida"
         });
       }
-      bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+
+      bcrypt.compare(req.body.password, user[0].password, function(
+        err,
+        result
+      ) {
         if (err) {
           return res.status(401).json({
             message: "Autenticacion invalida"
@@ -71,6 +96,37 @@ exports.users_login = (req, res) => {
           message: "Autenticacion invalida"
         });
       });
+
+      // bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+      //   if (err) {
+      //     return res.status(401).json({
+      //       message: "Autenticacion invalida"
+      //     });
+      //   }
+      //   if (result) {
+      //     const token = jwt.sign(
+      //       {
+      //         username: user[0].username,
+      //         userId: user[0]._id,
+      //         isAdmin: user[0].isAdmin
+      //       },
+      //       process.env.JWT_KEY,
+      //       {
+      //         expiresIn: "1h"
+      //       }
+      //     );
+      //     return res
+      //       .header("x-auth-token", token)
+      //       .header("access-control-expose-headers", "x-auth-token")
+      //       .status(200)
+      //       .json({
+      //         message: "Usuario autenticado !!"
+      //       });
+      //   }
+      //   return res.status(401).json({
+      //     message: "Autenticacion invalida"
+      //   });
+      // });
     })
     .catch(err => {
       console.log(err);
