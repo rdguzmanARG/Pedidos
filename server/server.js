7aa74bd8a57ae712b6a9031034ef504f0c41cbdf
Ricardo D. de Guzmán <rdguzman@tercerplaneta.com>
2020-02-21 11:31:01 -0300

    Remove useUnifiedTopology in the connection.

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ mongoose
   .connect(process.env.DB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true,
+    useUnifiedTopology: false,
     useFindAndModify: false
   })
   .then(() => {
