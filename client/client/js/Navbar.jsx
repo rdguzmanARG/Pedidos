da24353ffe8502cb49b2fae88efd72735357584e
Ricardo D. de Guzmán <rdguzman@tercerplaneta.com>
2020-02-24 16:31:07 -0300

    Change Name

--- client/client/js/Navbar.jsx
+++ client/client/js/Navbar.jsx
@@ -5,7 +5,7 @@ const NavBar = props => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <Link className="navbar-brand" to="/">
-        Sistemas de Pedidos
+        Pedidos
       </Link>
       <button
         className="navbar-toggler"
