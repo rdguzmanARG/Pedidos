c671df7d12ba1a0f1151a297656a80c02a7d209e
Ricardo D. de Guzmán <rdguzman@tercerplaneta.com>
2020-03-31 22:46:07 -0300

    Se implemento la carga de contactos desde el website.

--- website/client/js/components/quienesSomos.jsx
+++ website/client/js/components/quienesSomos.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Contact(props) {
+export default function QuienesSomos(props) {
   return (
     <div>
       <div class="jumbotron jumbotron-fluid">
