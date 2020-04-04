0aa7e4f5bbadddf2032ed972f88c9f5f8339b001
Ricardo D. de Guzmán <rdguzman@tercerplaneta.com>
2020-04-04 15:26:24 -0300

    Redirect from https

--- website/wwwroot/server.js
+++ website/wwwroot/server.js
@@ -6,10 +6,15 @@ const port = process.env.PORT || 5000;
 app.use("/", express.static(__dirname + "/"));
 
 app.get("/*", (req, res) => {
-  if (req.protocol == "https") {
-    res.redirect("http://" + req.hostname + req.path);
+  if (req.hostname != "localhost") {
+    if (req.protocol == "https") {
+      res.redirect("http://" + req.hostname + req.path);
+    } else {
+      res.sendFile(__dirname + "/index.html");
+    }
+  } else {
+    res.sendFile(__dirname + "/index.html");
   }
-  res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(port, () => {
