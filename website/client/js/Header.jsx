876bbda4512cbf60ab254ef312fe3fe063948329
Ricardo D. de Guzmán <rdguzman@tercerplaneta.com>
2020-04-22 18:58:52 -0300

    Se agrego un nuevo Banner para Anabela Rey

--- website/client/js/Header.jsx
+++ website/client/js/Header.jsx
@@ -24,7 +24,51 @@ class Header extends React.Component {
       {
         key: 1,
         html: (
-          <div className="romina-monzon">
+          <div className="banner anabela-rey">
+            <img
+              src="/images/adds/anabela-rey.png"
+              className="carousel-item-image"
+            ></img>
+            <div className="carousel-caption">
+              <h5>Lic. Anabela M. Rey</h5>
+              <h6>
+                Psicóloga Clínica - Docente{" "}
+                <span className="d-none d-sm-inline">
+                  - M.N. 40466 M.P. 72216
+                </span>
+              </h6>
+              <span className="d-block d-sm-none">M.N. 40466 M.P. 72216</span>
+              <span className="d-none d-sm-block d-lg-none">
+                Adolescentes/Adultos/Adultos mayores
+              </span>
+              <div className="carousel-caption-row">
+                <span className="d-none d-lg-block">
+                  Adolescentes/Adultos/Adultos mayores -
+                </span>
+                <span className="d-none d-sm-block">
+                  Arancel diferencial PAMI
+                </span>
+
+                <a
+                  target="_blank"
+                  onClick={() => this.clickBanner("anabela-rey")}
+                  href="https://api.whatsapp.com/send?phone=+5491165703197"
+                >
+                  11 6570-3197
+                </a>
+                <img
+                  className="whatsapp-icon"
+                  src="/images/Icons/whatsapp.png"
+                ></img>
+              </div>
+            </div>
+          </div>
+        ),
+      },
+      {
+        key: 2,
+        html: (
+          <div className="banner romina-monzon">
             <div
               className="carousel-item-image d-lg-none"
               style={{
@@ -68,6 +112,7 @@ class Header extends React.Component {
                   <span>Consultas sin cargo:</span>
                   <a
                     target="_blank"
+                    onClick={() => this.clickBanner("romina-monzon")}
                     href="https://api.whatsapp.com/send?phone=+5491163999959&text=Necesito asesoramiento contable"
                   >
                     116399-9959
@@ -83,54 +128,60 @@ class Header extends React.Component {
         ),
       },
       {
-        key: 2,
+        key: 3,
         html: (
-          <div
-            className="carousel-item-image"
-            style={{
-              backgroundImage: "url(" + "/images/banner-1.jpg" + ")",
-            }}
-          >
-            <div className="carousel-caption">
-              <h5>Ayudanos a crecer!!!!</h5>
-              <p className="d-none d-sm-block">
-                Te ayudamos a conseguir mas trabajos! Pone tu publicidad aqui!.
-              </p>
-              <Link
-                onClick={() => this.clickBanner("banner-1")}
-                to="/contactos"
-              >
-                Contactanos
-              </Link>
+          <div className="banner banner-general">
+            <div
+              className="carousel-item-image"
+              style={{
+                backgroundImage: "url(" + "/images/banner-1.jpg" + ")",
+              }}
+            >
+              <div className="carousel-caption">
+                <h5>Ayudanos a crecer!!!!</h5>
+                <p className="d-none d-sm-block">
+                  Te ayudamos a conseguir mas trabajos! Pone tu publicidad
+                  aqui!.
+                </p>
+                <Link
+                  onClick={() => this.clickBanner("banner-1")}
+                  to="/contactos"
+                >
+                  Contactanos
+                </Link>
+              </div>
             </div>
           </div>
         ),
       },
       {
-        key: 2,
+        key: 4,
         html: (
-          <div
-            className="carousel-item-image"
-            style={{
-              backgroundImage: "url(" + "/images/banner-2.jpg" + ")",
-            }}
-          >
-            <div className="carousel-caption">
-              <h5>Su oficio aquí!!!</h5>
-              <p className="d-none d-sm-block">
-                Hagase conocer a la comunidad a través de este espacio.
-              </p>
-              <Link
-                onClick={() => this.clickBanner("banner-2")}
-                to="/contactos"
-              >
-                Contactanos
-              </Link>
+          <div className="banner banner-general">
+            <div
+              className="carousel-item-image"
+              style={{
+                backgroundImage: "url(" + "/images/banner-2.jpg" + ")",
+              }}
+            >
+              <div className="carousel-caption">
+                <h5>Su oficio aquí!!!</h5>
+                <p className="d-none d-sm-block">
+                  Hagase conocer a la comunidad a través de este espacio.
+                </p>
+                <Link
+                  onClick={() => this.clickBanner("banner-2")}
+                  to="/contactos"
+                >
+                  Contactanos
+                </Link>
+              </div>
             </div>
           </div>
         ),
       },
     ].sort(() => 0.5 - Math.random());
+    //.filter((f) => f.key == 1);
 
     return (
       <header>
