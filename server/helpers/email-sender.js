2c40f30c8d8652ae6feb4426dca707ab0e4c8dc9
Ricardo D. de Guzmán <rdguzman@tercerplaneta.com>
2020-04-28 19:19:54 -0300

    Adjust Email template

--- server/helpers/email-sender.js
+++ server/helpers/email-sender.js
@@ -53,17 +53,229 @@ SendAllEmails = function (transporter, pedidos) {
       to: pe.email,
       subject: "Nodo Temperley - Pedido",
       html: `
-      <div>
-        <p>Hola ${pe.nombre} ${
-        pe.apellido
-      }, tu pedido al NODO Temperley, se esta procesado.</p>
-        <p>Podes ingresar al sitio y con este CODIGO: ${code
-          .substr(code.length - 5)
-          .toUpperCase()} y tu E-mail podes ver su estado, las veces que lo desees..</p>
-        <a href="http://nodo-temperley.azurewebsites.net/mi-pedido/${pe.idPedido.toString()}">ver pedido</a>
-        <p>Muchas Gracias</p>
-        <p>Nodo Temperley</p>
-      </div>`,
+      <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+      <html xmlns="http://www.w3.org/1999/xhtml">
+        <head>
+          <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+          <meta name="viewport" content="width=device-width" />
+      
+          <style type="text/css">
+            * {
+              margin: 0;
+              padding: 0;
+              font-size: 100%;
+              font-family: "Avenir Next", "Helvetica Neue", "Helvetica", Helvetica,
+                Arial, sans-serif;
+              line-height: 1.65;
+            }
+      
+            img {
+              max-width: 100%;
+              margin: 0 auto;
+              display: block;
+            }
+      
+            body,
+            .body-wrap {
+              width: 100% !important;
+              height: 100%;
+              background: #f8f8f8;
+            }
+      
+            a {
+              color: #71bc37;
+              text-decoration: none;
+            }
+      
+            a:hover {
+              text-decoration: underline;
+            }
+      
+            .text-center {
+              text-align: center;
+            }
+      
+            .text-right {
+              text-align: right;
+            }
+      
+            .text-left {
+              text-align: left;
+            }
+      
+            .button {
+              display: inline-block;
+              color: white;
+              background: #71bc37;
+              border: solid #71bc37;
+              border-width: 10px 20px 8px;
+              font-weight: bold;
+              border-radius: 4px;
+            }
+      
+            .button:hover {
+              text-decoration: none;
+            }
+      
+            h1,
+            h2,
+            h3,
+            h4,
+            h5, 
+            h7,
+            h6 {
+              margin-bottom: 20px;
+              line-height: 1.25;
+            }
+      
+            h1 {
+              font-size: 32px;
+            }
+      
+            h2 {
+              font-size: 28px;
+            }
+      
+            h3 {
+              font-size: 24px;
+            }
+      
+            h4 {
+              font-size: 20px;
+            }
+      
+            h5 {
+              font-size: 16px;
+            }
+            h7 {
+              font-size: 16px;
+              font-weight: bold;
+            }
+      
+            p,
+            ul,
+            ol {
+              font-size: 16px;
+              font-weight: normal;
+              margin-bottom: 20px;
+            }
+      
+            .container {
+              display: block !important;
+              clear: both !important;
+              margin: 0 auto !important;
+              max-width: 580px !important;
+            }
+      
+            .container table {
+              width: 100% !important;
+              border-collapse: collapse;
+            }
+      
+            .container .masthead {
+              padding: 80px 0;
+              background: #71bc37;
+              color: white;
+            }
+      
+            .container .masthead h1 {
+              margin: 0 auto !important;
+              max-width: 90%;
+              text-transform: uppercase;
+            }
+      
+            .container .content {
+              background: white;
+              padding: 30px 35px;
+            }
+      
+            .container .content.footer {
+              background: none;
+            }
+      
+            .container .content.footer p {
+              margin-bottom: 0;
+              color: #888;
+              text-align: center;
+              font-size: 14px;
+            }
+      
+            .container .content.footer a {
+              color: #888;
+              text-decoration: none;
+              font-weight: bold;
+            }
+      
+            .container .content.footer a:hover {
+              text-decoration: underline;
+            }
+          </style>
+        </head>
+        <body>
+          <table class="body-wrap">
+            <tr>
+              <td class="container">
+                <!-- Message start -->
+                <table>
+                  <tr>
+                    <td align="center" class="masthead">
+                      <h1>NODO Temperley</h1>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="content">
+                      <h2>Hola, ${pe.nombre} ${pe.apellido}</h2>
+      
+                      <p>
+                        Podes ingresar al sitio y 
+                        con este <h7>CODIGO: ${code
+                          .substr(code.length - 5)
+                          .toUpperCase()}</h7> y 
+                        tu <h7>E-mail</h7> así podras ver el estado de tu pedido las veces que lo desees, o haciendo click en el boton aquí abajo...            
+                      </p>
+                                     
+                      <table>
+                        <tr>
+                          <td align="center">
+                            <p>
+                              <a href="http://nodo-temperley.azurewebsites.net/mi-pedido/${pe.idPedido.toString()}" class="button">VER PEDIDO</a>
+                            </p>
+                          </td>
+                        </tr>
+                      </table>
+      
+                      <p>
+                        Si queres ver mas información 
+                        de LA CHAPANAY - Nodo Temperley hace click
+                        <a href="https://nodo-temperley.azurewebsites.net"> AQUI...</a>.
+                      </p>
+      
+                      <p><em>– Gracias!!</em></p>
+                    </td>
+                  </tr>
+                </table>
+              </td>
+            </tr>
+            <tr>
+              <td class="container">
+                <!-- Message start -->
+                <table>
+                  <tr>
+                    <td class="content footer" align="center">
+                      <p>
+                          © 2020 <a href="#">MrcGo</a>, 11-3208-9799 
+                      </p>
+                      <p>
+                        <a href="mailto:">ricardo.deguzman@gmail.com</a> 
+                      </p>
+                    </td>
+                  </tr>
+                </table>
+              </td>
+            </tr>
+          </table>
+        </body>
+      </html>`,
     };
 
     const prom = transporter
